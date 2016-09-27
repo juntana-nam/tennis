0ec2eccb03e5f4ecb068f1e39098399094e9fcd6
Lalitwadee Yodnin <57160482@go.buu.ac.th>
2016-09-27 14:59:54 +0700

    แก้ไข func scoreA เพิ่มการวนลูป และเพิ่ม func findScore

--- spec/tennis.spec.js
+++ spec/tennis.spec.js
@@ -1,18 +1,58 @@
 function Tennis(){
-    this.start  = function () {
-    };
-    this.echo = function(){
-      return 'Love - Love';
-    };
+  var scoreA;
+  var scoreB;
+
+  this.start = function(){
+    scoreA = 0;
+    scoreB = 0;
+  }
+
+  this.getScoreA = function(){
+    if(scoreA === 0 || scoreA === 15){
+      scoreA += 15;
+    }
+  };
+
+  var sum = 0;
+  var tmp = '';
+
+  this.findScore = function(){
+    sum = scoreA;
+    for(var i = 0; i<2; i++){
+      if(sum === 0){
+        tmp += 'Love';
+      }
+      else if(sum === 15){
+        tmp += 'Fifteen';
+      }
+
+      sum = scoreB;
+
+      }
+
+  };
+
+  this.echo = function(){
+    return tmp;
+  }
+}
+
+
 };
 
 describe('Tennis game', function(){
-  it('Should echo "Love - Love" when starting the game', function(){
-    var tennis = new Tennis(0,0);
+  it('Should echo "Love - Love" when Score A and Score B = 0 on the game', function(){
+    var tennis = new Tennis();
+    tennis.start();
+    tennis.findScore();
     expect(tennis.echo()).toEqual('Love - Love');
   });
-  it('Should echo "Fifty - Love" when Score A = 15 the game', function(){
-    var tennis = new Tennis(15,0);
-    expect(tennis.echo()).toEqual('Fifty - Love');
+
+  it('Should echo "Fifty - Love" when Score A = 15 on the game', function(){
+    var tennis = new Tennis();
+    tennis.start();
+    tennis.getScoreA();
+    tennis.findScore();
+    expect(tennis.echo()).toEqual('Fifteen - Love');
   });
   });
